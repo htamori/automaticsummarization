5d6fbd4945aeff2e25422e0e4b0f56287421c149
Hideaki Tamori <hideaki@DN0a2296a0.SUNet>
2015-11-12 12:11:36 -0800

    xmlparser

--- xmlparser.py
+++ xmlparser.py
@@ -1,39 +1,17 @@
-from xml.dom import minidom
+# -*- coding: utf-8 -*-
+from xml.etree.ElementTree import *
 
-doc = minidom.parse("1815430.xml")
+def xmlparser(filename):
+    tree = parse(filename) # 返値はElementTree型
+    elem = tree.getroot() # ルート要素を取得(Element型)
 
-def getNodeText(node):
+    ret = {}
+    fulltext = ""
+    for e in elem.findall(".//body.content/block[@class='full_text']/p"):
+        fulltext = fulltext + " " + e.text
+    ret["fulltext"] = fulltext
 
-    nodelist = node.childNodes
-    result = []
-    for node in nodelist:
-        if node.nodeType == node.TEXT_NODE:
-            result.append(node.data)
-    return ''.join(result)
+    ret["id"] = elem.find(".//docdata/doc-id").get("id-string")
+    ret["title"] = elem.find(".//body.head/hedline/hl1").text
 
-title = doc.getElementsByTagName("title")[0]
-print("Node Name : %s" % title.nodeName)
-print("Node Value : %s \n" % getNodeText(title))
-
-
-#bodycontents = doc.getElementsByTagName("body")
-headline = doc.getElementsByTagName("hl1")[0]
-print("Headline : %s \n" % getNodeText(headline))
-
-blockcontents = doc.getElementsByTagName("full_text")
-print len(blockcontents)
-for blockcontent in blockcontents:
-    print blockcontent
-    #fulltext = blockcontent.attributeName
-    print fulltext
-    #texts = fulltext.getElementsByTagName("p")[0]
-    #for text in texts:
-        #print text
-#for bodycontent in bodycontents:
-#       headline = bodycontent.getAttribute("hl1")
-        #sid = staff.getAttribute("id")
-        #nickname = staff.getElementsByTagName("nickname")[0]
-        #salary = staff.getElementsByTagName("salary")[0]
-        #print("id:%s, nickname:%s, salary:%s" %
-              #(sid, getNodeText(nickname), getNodeText(salary)))
-#        print headline
+    return ret
